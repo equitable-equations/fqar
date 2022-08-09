ed47244d89abafa2ea7524e0933ea9d4f4b33c05
equitable-equations <agard@lakeforest.edu>
2022-08-08 21:54:08 -0500

    align tests with new function output structures

--- tests/testthat/test-transect_list_glance.R
+++ tests/testthat/test-transect_list_glance.R
@@ -6,7 +6,7 @@ test_that("transect_list_glance works", {
   test_list <- download_transect_list(63, id %in% test_vec)
   test <- transect_list_glance(test_list)
 
-  expect_equal(ncol(test), 55)
+  expect_equal(ncol(test), 54)
   expect_equal(typeof(test$`Total Mean C`), "double")
   expect_equal(nrow(test), 2)
 })
