38fd516c10d0f6ec1ee63886cf2f3b6b9c134219
equitable-equations <agard@lakeforest.edu>
2022-08-11 13:08:16 -0500

    test transect_phys for manually-downloaded sets

--- tests/testthat/test-transect_phys.R
+++ tests/testthat/test-transect_phys.R
@@ -1,10 +1,30 @@
 test_that("transect_phys works", {
 
   expect_error(transect_phys("hi"))
+  expect_error(transect_phy(faithful))
 
-  test <- download_transect(25002)
-  test <- transect_phys(test)
-
+  test_raw <- download_transect(4492) # normal
+  test <- transect_phys(test_raw)
   expect_equal(ncol(test), 6)
-  expect_equal(typeof(test$Frequency), "double")
+  expect_equal(typeof(test[[2]]), "double")
+  expect_equal(names(test)[2], "Frequency")
+
+
+  test_raw <- download_transect(7025) # custom
+  test <- transect_phys(test_raw)
+  expect_equal(ncol(test), 6)
+  expect_equal(typeof(test[[2]]), "double")
+  expect_equal(names(test)[2], "Frequency")
+
+  test_raw <- download_transect(6444) # included omernik
+  test <- transect_phys(test_raw)
+  expect_equal(ncol(test), 6)
+  expect_equal(typeof(test[[2]]), "double")
+  expect_equal(names(test)[2], "Frequency")
+
+  test <- transect_phys(test_transect)
+  test <- transect_phys(test_raw)
+  expect_equal(ncol(test), 6)
+  expect_equal(typeof(test[[2]]), "double")
+  expect_equal(names(test)[2], "Frequency")
 })
