f56cc3fda9de3dcdad7979807090da1d43d15acc
equitable-equations <agard@lakeforest.edu>
2022-08-11 21:28:19 -0500

    Add code to generate data sets

---
+++ data-raw/data_raw_code.R
@@ -0,0 +1,14 @@
+# Code to build chicago data set
+
+chicago_list <- download_assessment_list(database_id = 149)
+chicago <- assessment_list_glance(chicago_list)
+
+use_this::use_data(chicago, overwrite = TRUE)
+
+
+# Code to build missouri data set
+
+missouri_list <- download_assessment_list(database_id = 63)
+missouri <- assessment_list_glance(missouri_list)
+
+use_this::use_data(missouri, overwrite = TRUE)
