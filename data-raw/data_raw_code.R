851fd05bb3fbbf18721087376041a15769455474
equitable-equations <agard@lakeforest.edu>
2022-08-11 22:02:22 -0500

    fix typo in code

--- data-raw/data_raw_code.R
+++ data-raw/data_raw_code.R
@@ -3,7 +3,7 @@
 chicago_list <- download_assessment_list(database_id = 149)
 chicago <- assessment_list_glance(chicago_list)
 
-use_this::use_data(chicago, overwrite = TRUE)
+usethis::use_data(chicago, overwrite = TRUE)
 
 
 # Code to build the missouri data set
@@ -11,4 +11,4 @@ use_this::use_data(chicago, overwrite = TRUE)
 missouri_list <- download_assessment_list(database_id = 63)
 missouri <- assessment_list_glance(missouri_list)
 
-use_this::use_data(missouri, overwrite = TRUE)
+usethis::use_data(missouri, overwrite = TRUE)
