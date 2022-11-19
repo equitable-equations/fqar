37ec5b96bf66ee51e8fe8ea22ba1929c9af96d25
equitable-equations <agard@lakeforest.edu>
2022-11-19 11:46:42 -0600

    Add species_profile()

---
+++ tests/testthat/test-species_profile.R
@@ -0,0 +1,28 @@
+test_that("species_profile works", {
+
+  species <- "Anemone canadensis"
+  bad_list1 <- list(faithful)
+  bad_list2 <- list(test_assessment, faithful)
+
+  expect_error(species_profile(species, bad_list1))
+  expect_error(species_profile(species, bad_list2))
+  expect_error(species_profile(species))
+
+  test_assessments <- list(test_assessment,
+                           test_assessment2,
+                           test_assessment_manual)
+  good_list <- assessment_list_inventory(test_assessments)
+
+  anemone <- species_profile("Anemone canadensis", good_list)
+  expect_equal(ncol(anemone), 4)
+  expect_equal(nrow(anemone), 11)
+  expect_equal(names(anemone)[4], "cospecies_n")
+  expect_equal(anemone$target_c[1], 4)
+
+  anemone <- species_profile("Anemone canadensis", good_list, native = TRUE)
+  expect_equal(ncol(anemone), 4)
+  expect_equal(nrow(anemone), 11)
+  expect_equal(names(anemone)[4], "cospecies_n")
+  expect_equal(anemone$target_c[1], 4)
+
+})
