804aaab1a1f5c163ae09f8734fe7e3d31933bc37
equitable-equations <agard@lakeforest.edu>
2023-12-30 11:52:19 -0600

    update examples

--- R/download_assessment.R
+++ R/download_assessment.R
@@ -20,19 +20,16 @@
 #' @importFrom memoise drop_cache
 #'
 #' @examples
-#' \donttest{
-#'
-#' databases <- index_fqa_databases()
-#' # Note database 1 is the original 1994 Chicago edition.
+#' databases <- index_fqa_databases() # Database 1 is the original 1994 Chicago edition.
 #'
 #' chicago_assessments <- index_fqa_assessments(1) # Edison dune and swale has id number 25002.
 #' edison <- download_assessment(25002)
 #'
 #' edison_tidy <- assessment_glance(edison)
 #' edison_species <- assessment_inventory(edison)
-#' }
 #'
 #' @export
+
 
 download_assessment <- function(assessment_id) {
 
