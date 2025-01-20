5022f7049b5fe9bf88ebfb3cd11987c7fe73a973
equitable-equations <agard@lakeforest.edu>
2025-01-19 18:07:26 -0600

    add assessment_id to internal test data sets

--- data-raw/internal_data_code.R
+++ data-raw/internal_data_code.R
@@ -5,12 +5,15 @@ library(usethis)
 
 test_assessment <- read_csv("data-raw/test_assessment.csv")
 test_assessment2 <- read_csv("data-raw/test_assessment2.csv")
-test_assessment_manual <- read_csv("data-raw/test_assessment_manual.csv",
-                                   skip_empty_rows = FALSE)
+test_assessment_manual <- read_delim("data-raw/test_assessment_manual.csv",
+                                     skip_empty_rows = FALSE,
+                                     delim = "***")
 
 test_transect <- read_csv("data-raw/test_transect.csv")
 
-test_database <- read_csv("data-raw/test_database.csv")
+test_database <- read_delim("data-raw/test_database.csv",
+                            delim = "***")
+test_database <- as.data.frame(test_database)
 
 test_inventory <- read_csv("data-raw/test_inventory.csv")
 
